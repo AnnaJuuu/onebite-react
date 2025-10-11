d44fed65ab576b0d0914a096f4105849fd17d937
Anna <a700hui@gmail.com>
2025-10-11 12:16:22 +0900

    Event Handling

--- react_practice/chapter03/src/components/Button.jsx
+++ react_practice/chapter03/src/components/Button.jsx
@@ -1,6 +1,16 @@
 const Button = ({ children, text, color = "black" }) => {
+  const onClickButton = (e) => {
+    console.log(e);
+    console.log(text);
+  };
+
   return (
-    <button style={{ color: color }}>
+    <button
+      // 실질적으로 처리하는 함수 -> 이벤트 핸들러
+      onClick={onClickButton}
+      // onMouseEnter={onClickButton}
+      style={{ color: color }}
+    >
       {text} - {color.toUpperCase()}
       {children}
     </button>
