228e8e36fc4c6eeaddeb8d5d0e516ea4705f2595
Anna <a700hui@gmail.com>
2025-10-12 11:20:38 +0900

    State로 사용자 입력 관리하기 1

--- react_practice/chapter06/src/App.jsx
+++ react_practice/chapter06/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+// import { useState } from "react";
+import Register from "./components/Register";
 
 function App() {
   return (
     <>
-      <Bulb />
-      <Counter />
+      <Register />
     </>
   );
 }
